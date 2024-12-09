82658831c06cc1373254f62bdc2fb1ba45a341fa
ishanvisrivastavaa <ishanvisrivastavaa7@gmail.com>
2024-12-09 12:00:17 +0530

    // updated play your move button and added reset button

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll( ".choice" );
-
-
+const msg = document.querySelector( "#msg" );
+const userScorePara = document.querySelector( "#user-score" );
+const compScorePara = document.querySelector("#comp-score");
+const resetBtn = document.querySelector( "#reset-btn" );
 
 const genCompChoice = () =>
 {
@@ -17,18 +19,28 @@ const genCompChoice = () =>
 const drawGame = () =>
 {
   console.log( "game was draw." );
+  msg.innerText = "Game was draw. Play again. ";
+  msg.style.backgroundColor = "grey";
 }
 
 
-const showWinner = ( userWin ) =>
+const showWinner = ( userWin,userChoice, compChoice ) =>
 {
-  if ( userWin)
+  if ( userWin )
   {
     console.log( "You Win!" );
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
   }
   else
   {
+    compScore++;
+    compScorePara.innerText = compScore;
     console.log( "You Lose!" );
+    msg.innerText = `You Lose! ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
   }
 }
 
@@ -65,7 +77,7 @@ const playGame = (userChoice) =>
       //computer choices -> rock,paper
       userWin = compChoice === "rock" ? false : true;
     }
-    showWinner( userWin );
+    showWinner( userWin,userChoice,compChoice );
   }
 }
 
@@ -79,4 +91,20 @@ choices.forEach( ( choice ) =>
     // console.log( "choice was clicked",userChoice);
     playGame( userChoice );
   })
-})
+} )
+
+
+
+const resetGame = () =>
+{
+  userScore = 0;
+  compScore = 0;
+  userScorePara.innerText = 0;
+  compScorePara.innerText = 0;
+  msg.innerText = "Play your move!";
+  msg.style.backgroundColor = "white";
+  
+  console.log( "Game has been reset." );
+}
+
+resetBtn.addEventListener( "click", resetGame );
